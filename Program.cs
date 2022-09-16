51e76f482de2ca7bf31b2c897f4fa4bf0403b4b8
Koffepomp <Zephs1986@gmail.com>
2022-09-16 15:58:23 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+// RECIPE PRINTER
+//- Användaren ska få feedback från programmet att mata in en ingredienser till en lista:
+// Varje ingrediens ska sparas i en lista med strängar.
+//
+// - Ge användaren feedback (på ett snyggt sätt) att, om den är färdig, ska den skriva “Finished”
+// (eller kanske bara trycka på enter? Eller något annat?) för att gå vidare.
+//
+// - När användaren valt att gå vidare ska alla ingredienser användaren skrivit in i tidigare
+// steg printas till konsollen:
+// Använd en for each -loop för att printa alla ingredienser till konsollen
+//
+// - Ge användaren möjlighet att ta bort en ingrediens, om den skriver t.ex. “Remove” i konsollen…
+//
+// - Ge även användaren möjligheten att printa ingredienserna när den vill, inte bara när den är färdig
+// och ge den även möjligheten att, efter att den printat alla ingredienser, gå tillbaka och
+// lägga till/ta bort saker från listan igen.
+//
+// Superbonus: Ge användaren en prompt att även fylla i vilken kvantitet som varje specifik ingrediens
+// ska ha, och spara denna i en lista med integers eller floats (inte strängar)! Du kan även använda
+// Dictionaries, men då får du kolla upp lite själv (se “Resurser” nedan)
+
+// --------------------------------------------------------------------------------------------------
